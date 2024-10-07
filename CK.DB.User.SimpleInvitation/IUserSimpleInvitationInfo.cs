4322345786dc13ff63589da33db59972227871f3
Olivier Spinelli <olivier.spinelli@signature-code.com>
2024-10-07 08:24:11 +0200

    Applying Code Cleanup.

--- CK.DB.User.SimpleInvitation/IUserSimpleInvitationInfo.cs
+++ CK.DB.User.SimpleInvitation/IUserSimpleInvitationInfo.cs
@@ -1,54 +1,53 @@
 using CK.Core;
 using System;
 
-namespace CK.DB.User.SimpleInvitation
+namespace CK.DB.User.SimpleInvitation;
+
+/// <summary>
+/// Base model of an invitation.
+/// </summary>
+public interface IUserSimpleInvitationInfo : IPoco
 {
     /// <summary>
-    /// Base model of an invitation.
+    /// Gets the invitation identifier.
+    /// This property should only be read from the database.
     /// </summary>
-    public interface IUserSimpleInvitationInfo : IPoco
-    {
-        /// <summary>
-        /// Gets the invitation identifier.
-        /// This property should only be read from the database.
-        /// </summary>
-        int InvitationId { get; set; }
+    int InvitationId { get; set; }
 
-        /// <summary>
-        /// Gets the invitation token to use.
-        /// This property should only be read from the database.
-        /// </summary>
-        string InvitationToken { get; set; }
+    /// <summary>
+    /// Gets the invitation token to use.
+    /// This property should only be read from the database.
+    /// </summary>
+    string InvitationToken { get; set; }
 
-        /// <summary>
-        /// The email of the invited user.
-        /// This is the key for an invitation: only one invitation can exist by EMail.
-        /// </summary>
-        string? EMail { get; set; }
+    /// <summary>
+    /// The email of the invited user.
+    /// This is the key for an invitation: only one invitation can exist by EMail.
+    /// </summary>
+    string? EMail { get; set; }
 
-        /// <summary>
-        /// The expiration date. Must always be in the future.
-        /// </summary>
-        DateTime ExpirationDateUtc { get; set; }
+    /// <summary>
+    /// The expiration date. Must always be in the future.
+    /// </summary>
+    DateTime ExpirationDateUtc { get; set; }
 
-        /// <summary>
-        /// Gets the number of invitation sent so far.
-        /// This property should only be read from the database.
-        /// </summary>
-        int InvitationSendCount { get; set; }
+    /// <summary>
+    /// Gets the number of invitation sent so far.
+    /// This property should only be read from the database.
+    /// </summary>
+    int InvitationSendCount { get; set; }
 
-        /// <summary>
-        /// Gets the last time (Utc) this invitation has been sent.
-        /// Defaults to <see cref="Util.UtcMinValue"/>.
-        /// This property should only be read from the database.
-        /// </summary>
-        DateTime LastInvitationSendDate { get; set; }
+    /// <summary>
+    /// Gets the last time (Utc) this invitation has been sent.
+    /// Defaults to <see cref="Util.UtcMinValue"/>.
+    /// This property should only be read from the database.
+    /// </summary>
+    DateTime LastInvitationSendDate { get; set; }
 
-        /// <summary>
-        /// Gets or sets an optional binary options that may
-        /// contain invitation specific data
-        /// </summary>
-        byte[]? Options { get; set; }
+    /// <summary>
+    /// Gets or sets an optional binary options that may
+    /// contain invitation specific data
+    /// </summary>
+    byte[]? Options { get; set; }
 
-    }
 }
--- CK.DB.User.SimpleInvitation/Package.cs
+++ CK.DB.User.SimpleInvitation/Package.cs
@@ -1,16 +1,15 @@
 using CK.Core;
 
-namespace CK.DB.User.SimpleInvitation
+namespace CK.DB.User.SimpleInvitation;
+
+/// <summary>
+/// Package that supports simple user invitation. 
+/// </summary>
+[SqlPackage( Schema = "CK", ResourcePath = "Res" )]
+[Versions( "1.0.0" )]
+public abstract class Package : SqlPackage
 {
-    /// <summary>
-    /// Package that supports simple user invitation. 
-    /// </summary>
-    [SqlPackage( Schema = "CK", ResourcePath = "Res" )]
-    [Versions("1.0.0")]
-    public abstract class Package : SqlPackage
+    void StObjConstruct( Actor.Package actor )
     {
-        void StObjConstruct( Actor.Package actor )
-        {
-        }
     }
 }
